86fedc3c662246107e432cf952495508d8342ac6
sandeepmaharjan55 <sandeepmaharjan55@gmail.com>
2018-05-13 13:04:01 +0545

    till skill matched

--- FirstTask_Project/Controllers/RecruitController.cs
+++ FirstTask_Project/Controllers/RecruitController.cs
@@ -219,55 +219,27 @@ namespace FirstTask_Project.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult SkillMatch(int? id, string q, string order)
+        public ActionResult SkillMatch(int? id, string q)
         {
-
-
-            var name = from n in db.PersonToSkills.Include(r => r.Person).Include(r=>r.Skill) select n;
-            if (q != null)
-            {
-                name = name.Where(n => n.Person.Name.Contains(q));
-            }
-            switch (order)
-            {
-                case "name":
-                    name = name.OrderBy(n => n.Person.Name);
-                    break;
-                case "skill":
-                    name = name.OrderBy(n => n.Skill.Skills);
-                    break;
-                case "email":
-                    name = name.OrderBy(n => n.Person.Email);
-                    break;
-                case "exp":
-                    name = name.OrderBy(n => n.Person.Experience.ExpYear);
-                    break;
-                
-                default:
-                    name = name.OrderBy(n => n.PersonId);
-                    break;
-            }
-            return View(name.ToList());
-
-
-
-
-
-
-
-
 
             var rat = db.RecruitmentRequests.Find(id);
             string z = rat.Description;
-            
+
             var persontoskill = db.Skills
                        .Where(c => c.Skills == z)
                        .SelectMany(c => c.PersonToSkills);
+
+
+
+
+
+
+
+
+
+          
+                return View(persontoskill.ToList());
             
-                return View(persontoskill.Include(r => r.Person.Experience).ToList());
-
-
-
         }
 
         // GET: Person/Details/5
